ced9ce8db348a4c6abea24d491461ab35dc488c0
khalidur-sujon <khalidur.sujon@gmail.com>
2023-07-10 22:23:33 +0600

    Adding password weak/medium/strong tag dbasedon password

--- hideAndShowPassword/main.js
+++ hideAndShowPassword/main.js
@@ -1,7 +1,31 @@
 let password = document.getElementById("password");
 let toggoleImg = document.getElementById("toggle-img");
+let msg = document.getElementById("message");
+let str = document.getElementById("strength");
 
-toggoleImg.onclick = function () {
+password.addEventListener("input", () => {
+	if (password.value.length > 0) {
+		msg.style.display = "block";
+	} else {
+		msg.style.display = "none";
+	}
+
+	if (password.value.length < 4) {
+		str.innerHTML = "weak";
+		password.parentNode.style.borderColor = "orange";
+		msg.style.color = "orange";
+	} else if (password.value.length >= 4 && password.value.length < 8) {
+		str.innerHTML = "medium";
+		password.parentNode.style.borderColor = "yellow";
+		msg.style.color = "yellow";
+	} else {
+		str.innerHTML = "strong";
+		password.parentNode.style.borderColor = "green";
+		msg.style.color = "green";
+	}
+});
+
+toggoleImg.onclick = () => {
 	if (password.type == "password") {
 		password.type = "text";
 		toggoleImg.src = "./images/eye-open.png";
